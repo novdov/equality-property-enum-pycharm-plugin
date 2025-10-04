e0806c9ec949efef2ae2589b2b8174147b6b9215
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-10-05 00:53:23 +0900

    Update project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "equality-property-enum-plugin"
+rootProject.name = "equality-property-enum-pycharm-plugin"
